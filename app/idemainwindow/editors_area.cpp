6959e269b5ab4bb44b6eae28e3cc9a124c4083f3
Guilhem Vallat <guilhem.vallat@gmail.com>
2014-09-07 01:37:28 +0200

    EditorsArea: Use RecursiveSplitter (test)

--- app/idemainwindow/editors_area.cpp
+++ app/idemainwindow/editors_area.cpp
@@ -1,11 +1,25 @@
 #include "editors_area.hpp"
 
+#include <QVBoxLayout>
+
+#include <ide_widget/closable_widget.hpp>
+#include <ide_widget/recursive_splitter.hpp>
+
 namespace IDE
 {
 
 EditorsArea::EditorsArea(QWidget *parent)
   : QWidget(parent)
 {
+  ClosableWidget* cw = new ClosableWidget;
+
+  RecursiveSplitter* rs = new RecursiveSplitter;
+  rs->addWidget(cw);
+  cw->installEventFilter(rs);
+
+  QVBoxLayout* lyt = new QVBoxLayout(this);
+  lyt->setMargin(0);
+  lyt->addWidget(rs);
 }
 
 } // namespace IDE
