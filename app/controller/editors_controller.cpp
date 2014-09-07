6480ea1cb9d744af4ca7be42766c7a7348a15a9e
Guilhem Vallat <guilhem.vallat@gmail.com>
2014-09-07 10:01:13 +0200

    EditorsController: Implement event filter

--- app/controller/editors_controller.cpp
+++ app/controller/editors_controller.cpp
@@ -1,11 +1,79 @@
 #include "editors_controller.hpp"
 
+#include <ide_widget/editor/recursive_splitter.hpp>
+#include <ide_widget/commun/closable_widget.hpp>
+#include <ide_widget/commun/closable_widget_event.hpp>
+
 namespace IDE
 {
 
-EditorsController::EditorsController(QObject *parent)
+EditorsController::EditorsController(EditorsArea* editorsArea, QObject *parent)
   : QObject(parent)
+  , _editorsArea(editorsArea)
 {
+  ClosableWidget* cw = new ClosableWidget(ClosableWidget::SplitAndNewWindow);
+  _editorsArea->addWidget(cw);
+  cw->installEventFilter(this);
+}
+
+bool EditorsController::eventFilter(
+  QObject* watched,
+  QEvent* event)
+{
+  if(event->type() == ClosableWidgetEvent::type())
+  {
+    ClosableWidgetEvent* cwEvent = static_cast<ClosableWidgetEvent*>(event);
+    ClosableWidget* cw = dynamic_cast<ClosableWidget*>(watched);
+    RecursiveSplitter* rs = dynamic_cast<RecursiveSplitter*>(watched->parent());
+    if(cw && rs)
+    {
+      rs = rs->rootRecursiveSplitter();
+
+      if(cwEvent->requestedAction() == ClosableWidgetEvent::Unsplit)
+      {
+        if(rs->count() > 1)
+          if(rs->removeWidget(cw))
+            delete cw;
+      }
+      else if(cwEvent->requestedAction() == ClosableWidgetEvent::HorizontalSplit ||
+              cwEvent->requestedAction() == ClosableWidgetEvent::VerticalSplit)
+      {
+        Qt::Orientation orientation = Qt::Horizontal;
+        if(cwEvent->requestedAction() == ClosableWidgetEvent::VerticalSplit)
+          orientation = Qt::Vertical;
+        ClosableWidget* cw2 = new ClosableWidget(ClosableWidget::SplitAndNewWindow);
+        RecursiveIndex index = rs->indexOf(cw);
+        if(rs->insertWidget(index, cw2, orientation))
+        {
+          cw2->installEventFilter(this);
+        }
+        else
+        {
+          delete cw2;
+        }
+      }
+      else if(cwEvent->requestedAction() == ClosableWidgetEvent::NewWindow)
+      {
+        ClosableWidget* cw2 = new ClosableWidget(ClosableWidget::SplitAndNewWindow);
+        cw2->installEventFilter(this);
+
+        RecursiveSplitter* rs = new RecursiveSplitter;
+        rs->addWidget(cw2);
+        rs->show();
+
+        connect(rs, SIGNAL(destroyed(QObject*)), this, SLOT(xWindowDestroyed(QObject*)));
+        _editorsAreaWindows.append(rs);
+      }
+    }
+    return true;
+  }
+  else
+    return QObject::eventFilter(watched, event);
+}
+
+void EditorsController::xWindowDestroyed(QObject* window)
+{
+  _editorsAreaWindows.removeOne(static_cast<RecursiveSplitter*>(window));
 }
 
 } // namespace IDE
--- app/controller/editors_controller.hpp
+++ app/controller/editors_controller.hpp
@@ -3,15 +3,29 @@
 
 #include <QObject>
 
-namespace IDE {
+#include <QList>
+
+namespace IDE
+{
+class RecursiveSplitter;
+typedef RecursiveSplitter EditorsArea;
 
 class EditorsController : public QObject
 {
   Q_OBJECT
 public:
-  explicit EditorsController(QObject *parent = NULL);
+  explicit EditorsController(EditorsArea* editorsArea, QObject *parent = NULL);
 
+  virtual bool eventFilter(
+      QObject* watched,
+      QEvent* event);
 
+private Q_SLOTS:
+  void xWindowDestroyed(QObject* window);
+
+private:
+  EditorsArea* _editorsArea;
+  QList<RecursiveSplitter*> _editorsAreaWindows;
 };
 
 } // namespace IDE
